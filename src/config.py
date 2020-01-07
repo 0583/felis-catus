bc558d066be89d956eb6c0f333330a883f594d3a
yuetsin <akaza_akari@sjtu.edu.cn>
2020-01-07 15:06:12 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -7,8 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['DEBUG'] = DEBUG
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%s:@%s/%s' % (
-    USER_NAME, 'localhost', DB_NAME)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%s:%s@%s/%s' % (
+    USER_NAME, PASSWORD, 'localhost', DB_NAME)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 db = SQLAlchemy(app)
