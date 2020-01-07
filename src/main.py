093cbfccf720ea85c7530cd786a10d83b4023189
yuetsin <akaza_akari@sjtu.edu.cn>
2020-01-07 23:42:28 +0800

    [add] system pressure info

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 import json
 import time
+import psutil
 import random
 
 import psycopg2
@@ -38,6 +39,47 @@ def error_response():
     }
 
 
+def __getinfo(cpu_interval=1):
+    try:
+        cpu_pressure = psutil.cpu_percent(cpu_interval)
+        cpu_logical_count = psutil.cpu_count()
+        cpu_physical_count = psutil.cpu_count(logical=False)
+
+        virtual_mem = psutil.virtual_memory()
+        swap_mem = psutil.swap_memory()
+
+        return {
+            'status': 'ok',
+            'cpu': {
+                'pressure': cpu_pressure,
+                'logical_core': cpu_logical_count,
+                'physical_core': cpu_physical_count
+            },
+            'virtual_mem': {
+                # svmem(total=8589934592, available=2866520064, percent=66.6, used=7201386496, free=216178688, active=3342192640, inactive=2650341376, wired=1208852480)
+                'total': virtual_mem[0],
+                'available': virtual_mem[1],
+                'percent': virtual_mem[2],
+                'used': virtual_mem[3],
+                'free': virtual_mem[4],
+                'active': virtual_mem[5],
+                'inactive': virtual_mem[6],
+                'wired': virtual_mem[7]
+            },
+            'swap_mem': {
+                # sswap(total=1073741824, used=150732800, free=923009024, percent=14.0, sin=10705981440, sout=40353792)
+                'total': swap_mem[0],
+                'used': swap_mem[1],
+                'free': swap_mem[2],
+                'percent': swap_mem[3],
+                'sin': swap_mem[4],
+                'sout': swap_mem[5]
+            }
+        }
+    except:
+        return error_response()
+
+
 def get_line_count():
     global conn
     if conn:
@@ -69,10 +111,19 @@ YEAR = 2020
 MAX_RANDOM_COUNT = 5
 @app.route('/', methods=['GET'])
 def mainView():
+    info = __getinfo()
+    if info['status'] == 'ok':
+        cpu_p = "%d%% (%d logical / %d physical core[s])" % (
+            info['cpu']['pressure'], info['cpu']['logical_core'], info['cpu']['physical_core'])
+        mem_p = "virtual: %d%% used of %d MiB total; swap: %d%% used of %d MiB total" % (
+            info['virtual_mem']['percent'], int(info['virtual_mem']['total'] / (1024 * 1024)), info['swap_mem']['percent'], int(info['swap_mem']['total'] / (1024 * 1024)))
+    else:
+        cpu_p = '[temporarily unavailable]'
+        mem_p = '[temporarily unavailable]'
     line_count = get_line_count()
     random_count = min(MAX_RANDOM_COUNT, line_count)
     if random_count == 0:
-        return render_template('main_empty.tpl', title=TITLE, main_intro=INTRO, table_title=TABLE_TITLE, year=YEAR)
+        return render_template('main_empty.tpl', title=TITLE, main_intro=INTRO, table_title=TABLE_TITLE, year=YEAR, cpu_pressure=cpu_p, memory_pressure=mem_p)
     else:
         TABLE = []
         ints = list(range(1, line_count + 1))
@@ -80,10 +131,10 @@ def mainView():
         for v in range(0, random_count):
             i = ints[v]
             peek = __peek(hex(crc32(bytes(str(i), 'utf-8')))[2:].rjust(8, '0'))
-            if 'full' in peek:
+            if peek['status'] == 'ok':
                 TABLE.append(
                     ("#%d" % (i + 1), peek['short'], peek['full'], "./t/%s" % peek['short']))
-        return render_template('main.tpl', title=TITLE, main_intro=INTRO, table_title=TABLE_TITLE, table=TABLE, year=YEAR)
+        return render_template('main.tpl', title=TITLE, main_intro=INTRO, table_title=TABLE_TITLE, table=TABLE, year=YEAR, cpu_pressure=cpu_p, memory_pressure=mem_p)
 
 
 def __peek(short_link):
@@ -111,12 +162,18 @@ def __peek(short_link):
 
     if response and len(response) == 3:
         resp = {
+            'status': 'ok',
             'short': short_link,
             'full': response[2]
         }
         return resp
     else:
         return error_response()
+
+
+@app.route('/info', methods=['GET'])
+def GetInfo():
+    return jsonify(__getinfo())
 
 
 @app.route('/peek/<short_link>', methods=['GET'])
