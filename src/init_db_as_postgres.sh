6dfede6730ef5e694eca3f489664a35f8bdecdf4
yuetsin <akaza_akari@sjtu.edu.cn>
2020-01-07 15:01:43 +0800

    [fix] missing --username

--- src/init_db_as_postgres.sh
+++ src/init_db_as_postgres.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash 
 
-sudo -u postgres createuser -h localhost -p 5432 felis --password 123456
+sudo -u postgres createuser -h localhost -p 5432 --username felis --password 123456
 sudo -u postgres createdb -O felis felisdb
