a7d04b90594a92cdd4fe41c33fde3a82301e5487
Yuan-Zhuo <yuanzhuo0118@outlook.com>
2020-01-07 23:14:56 +0800

    [add] username

--- cluster/get_env.sh
+++ cluster/get_env.sh
@@ -1,3 +1,7 @@
+# transfer
+sudo apt-get update
+sudo apt-get install lrzsz
+
 # docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 chmod +x get-docker.sh
@@ -19,10 +23,10 @@ echo "docker-compose installed"
 
 # kube
 sudo groupadd microk8s
-sudo usermod -a -G microk8s ubuntu
 curl https://raw.githubusercontent.com/ycheng/microk8s-kubeflow-install/master/microk8s-install.bash > microk8s-install.sh
 chmod + microk8s-install.sh
 sudo ./microk8s-install.sh
+sudo usermod -a -G microk8s ubuntu
 microk8s.status --wait-ready
 
 echo "microkube installed"
