4fb51f42874d890453b1af1add36b56e6cea2687
Yuan-Zhuo <yuanzhuo0118@outlook.com>
2020-01-07 22:36:08 +0800

    [fix] cmd

--- cluster/get_env.sh
+++ cluster/get_env.sh
@@ -11,8 +11,16 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
+# kube
+sudo groupadd microk8s
+sudo usermod -a -G microk8s ubuntu
+curl https://raw.githubusercontent.com/ycheng/microk8s-kubeflow-install/master/microk8s-install.bash > microk8s-install.sh
+chmod + microk8s-install.sh
+./microk8s-install.sh
+microk8s.status --wait-ready
+
 # file
-curl https://raw.githubusercontent.com/0583/felis-catus/cluster/docker/docker-compose.yml >> docker-compose.yml
-curl https://raw.githubusercontent.com/0583/felis-catus/cluster/docker/run_docker_compose.sh >> run_docker_compose.sh
+curl https://raw.githubusercontent.com/0583/felis-catus/cluster/docker/docker-compose.yml > docker-compose.yml
+curl https://raw.githubusercontent.com/0583/felis-catus/cluster/docker/run_docker_compose.sh > run_docker_compose.sh
 chmod +x run_docker_compose.sh
-./run_docker_compose.sh
+./run_docker_compose.sh &
--- docker/run_docker_compose.sh
+++ docker/run_docker_compose.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash 
 docker-compose down
-docker pull docker.pkg.github.com/0583/felis-catus/felis-catus-db:latest
-docker pull docker.pkg.github.com/0583/felis-catus/felis-catus-server:latest
+docker pull yuxiqian/felis-catus-db:latest
+docker pull yuxiqian/felis-catus-server:latest
 docker-compose up
