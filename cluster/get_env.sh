0f8f6a555cdab4e811987c1bde99fbc4867bcb44
Yuan-Zhuo <yuanzhuo0118@outlook.com>
2020-01-07 20:38:39 +0800

    [fix] url

--- cluster/get_env.sh
+++ cluster/get_env.sh
@@ -12,7 +12,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # file
-curl https://github.com/0583/felis-catus/blob/cluster/docker/docker-compose.yml >> docker-compose.yml
-curl https://github.com/0583/felis-catus/blob/cluster/docker/run_docker_compose.sh >> run_docker_compose.sh
+curl https://raw.githubusercontent.com/0583/felis-catus/cluster/docker/docker-compose.yml >> docker-compose.yml
+curl https://raw.githubusercontent.com/0583/felis-catus/cluster/docker/run_docker_compose.sh >> run_docker_compose.sh
 chmod +x run_docker_compose.sh
 ./run_docker_compose.sh
