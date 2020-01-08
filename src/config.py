3c24401da08a60adce4f476151aec32e7850f2a6
yuetsin <akaza_akari@sjtu.edu.cn>
2020-01-08 14:05:50 +0800

    [add] locust scripts

--- src/config.py
+++ src/config.py
@@ -6,9 +6,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+LOCAL_RUN = False
+if LOCAL_RUN:
+    HOST = 'localhost'
+else:
+    HOST = 'felisdb'
+
 app.config['DEBUG'] = DEBUG
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%s:%s@%s/%s' % (
-    USER_NAME, PASSWORD, 'localhost', DB_NAME)
+    USER_NAME, PASSWORD, HOST, DB_NAME)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 db = SQLAlchemy(app)
